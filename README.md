a3d3c81b21bab949a656b742ea78791ae2f3e87d
Mahesh Vaidya <forvaidy@gmail.com>
2024-09-03 07:52:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ + docker login -u forvaidya (PAT is there on my laptop)
+ + docker build -t forvaidya/colored-flask .
+ + docker push forvaidya/colored-flask
---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+color_array=['#FF0000', '#00FF00']
+
+random_number = random.randint(0, 1)
+final_color=color_array[random_number]
+
+@app.route('/')
+def home():
+    return render_template('index.html', final_color=final_color)
+
+if __name__ == '__main__':
+    app.run(debug=False)
