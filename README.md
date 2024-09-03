51d6a9f1ceb29b32d92804bc1f326558b013d976
Mahesh Vaidya <forvaidy@gmail.com>
2024-09-03 14:06:20 +0530

    better

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
  Test command 
 
- docker run --rm -d --name mahesh-colored-flask -p 8080:8080 --net=host colored-flask
+ docker run --rm -d --name mahesh-colored-flask -p 8080:8080 --net=host forvaidya/colored-flask
  (port 5000 is used somewhere on my mac) 
 
  kubectl delete service/colored-flask ; kubectl delete deployment/colored-flask
+ docker system prune --all
--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 import random
 app = Flask(__name__)
 
@@ -6,10 +7,15 @@ color_array=['#FF0000', '#00FF00']
 
 random_number = random.randint(0, 1)
 final_color=color_array[random_number]
+pod_name = None
+try:
+    pod_name=os.environ['MY_POD_NAME']
+except:
+    pass
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', final_color=final_color)
-
+    return render_template('index.html', pod_name=pod_name, final_color=final_color)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
