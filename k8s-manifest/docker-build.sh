871994ea2b232237f081f164ecc30e6c04234f91
Mahesh Vaidya <mahesh.vaidya@tpv-tech.com>
2024-09-05 10:58:38 +0530

    working snap

--- k8s-manifest/docker-build.sh
+++ k8s-manifest/docker-build.sh
@@ -1,9 +1,16 @@
-git commit -am "build commit" 
-git push origin
+
+EXTRA_NAME=$1
+
 x=$(git rev-parse HEAD)
 git_sha=${x:0:6}
 
-docker login -u forvaidya
+echo -n "${DOCKER_PAT}" | docker login -u forvaidya  --password-stdin
 docker build -t forvaidya/colored-flask:latest  ../src
 docker tag forvaidya/colored-flask:latest forvaidya/colored-flask:${git_sha}
+
+if [[ $EXTRA_NAME ]]
+then
+    docker tag forvaidya/colored-flask:latest forvaidya/colored-flask:${EXTRA_NAME}
+fi 
+
 docker push forvaidya/colored-flask --all-tags
